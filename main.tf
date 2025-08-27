82fbb9dad93036f2d29e8f65f0ca2958886d40a4
rosel <rosel@rosel>
2025-08-27 13:02:08 +0900

    add policy

--- main.tf
+++ main.tf
@@ -14,3 +14,24 @@ resource "aws_iam_role" "name" {
     ]
   })
 }
+
+resource "aws_iam_policy" "name" {
+  name = "terraform-policy-s3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:ListAllMyBuckets", "s3:ListBucket", "s3:HeadBucket"]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "name" {
+  name       = "terraform-attach-policy"
+  roles      = [aws_iam_role.name.name]
+  policy_arn = aws_iam_policy.name.arn
+}
