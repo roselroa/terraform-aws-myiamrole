993ae7fd52e38352381d70ccacba1c160281580a
rosel <rosel@rosel>
2025-08-27 13:13:55 +0900

    added provider for testing

--- variable.tf
+++ variable.tf
@@ -3,3 +3,9 @@ variable "role_name" {
   description = "The name of the IAM role"
   default     = "example-role"
 }
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to deploy resources in Tokyo"
+  default     = "ap-northeast-1"
+}
